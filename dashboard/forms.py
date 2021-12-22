7dc2d96eb8059be31593a186c7194c7faba6e9fd
yusei-zzz <tonicz1727@gmail.com>
2021-12-22 15:19:50 +0900

    basa.html,index.html,userupdate.html,userupdate.htmlを変更,grd.svg,signin.cssを追加

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -1,5 +1,13 @@
 from django import forms
 
 class LoginForm(forms.Form):
-    student_id = forms.CharField(label='学籍番号(12345678)',max_length=20)
-    passwd = forms.CharField(label='パスワード',max_length=20)
+    student_id = forms.CharField(label='',max_length=20,
+        widget=forms.TextInput(
+            attrs={'placeholder':'学籍番号(12345678)', 'class':'form-control'}))
+    passwd = forms.CharField(label='',max_length=20,
+       widget=forms.TextInput(
+            attrs={'placeholder':'パスワード', 'class':'form-control'}))
+
+    # student_id = forms.CharField(label='学籍番号',max_length=20)
+    # passwd = forms.CharField(label='パスワード',max_length=20)
+     
